e4a9d9f737267a5f23ea8a21b6bd82fe81868992
sydney flak <sydlaurenf@gmail.com>
2024-12-22 09:03:38 -0800

    health checker

---
+++ src/components/Neat/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useState } from 'react';
+import { Button } from '@ui/button';
+import Flex from '@ui/Flex';
+import useHealthCheck from '@/hooks/useHealthCheck';
+
+const Neat = () => {
+  const [count, setCount] = useState(0);
+
+  useHealthCheck();
+
+  return (
+    <Flex gap={2} className="w-full">
+      <Button onClick={() => setCount((prev) => prev + 1)}>Neat!</Button>
+      <p>{`Neat counter: ${count}`}</p>
+    </Flex>
+  );
+};
+
+export default Neat;
